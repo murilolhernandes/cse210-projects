45cd8563a5e13f3c7a5821c031a18e52a538b8b0
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-02-03 17:13:53 -0700

    finished learning assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Console.WriteLine("Hello World! This is the Homework Project.");
+        // Assignment assignment1 = new Assignment("Murilo Hernandes", "C#");
+        // Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Murilo Hernandes", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Murilo Hernandes", "European History", "The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
