14937d24b999d0f864eec158c39b33c6af02578c
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-02-10 10:17:49 -0700

    Finished learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("yellow", 5);
+        shapes.Add(square1);
+        // Console.WriteLine(square1.GetColor());
+        // Console.WriteLine(square1.GetArea());
+
+        Rectangle rectangle1 = new Rectangle("blue", 10, 8);
+        shapes.Add(rectangle1);
+        // Console.WriteLine(rectangle1.GetColor());
+        // Console.WriteLine(rectangle1.GetArea());
+
+        Circle circle1 = new Circle("red", 4);
+        // Console.WriteLine(circle1.GetColor());
+        // Console.WriteLine(circle1.GetArea());
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,21 @@
+public abstract class Shape
+{
+  private string _color;
+
+  public Shape(string color)
+  {
+    _color = color;
+  }
+
+  public string GetColor()
+  {
+    return _color;
+  }
+
+  protected void SetColor(string color)
+  {
+    _color = color;
+  }
+
+  public abstract double GetArea();
+}
